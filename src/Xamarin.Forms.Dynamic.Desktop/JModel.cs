893534d57807d18e64e78e2c9189ea6a15f6f287
Daniel Cazzulino <daniel@cazzulino.com>
2015-04-25 18:18:25 -0300

    Added desktop behavior for JModel

--- src/Xamarin.Forms.Dynamic.Desktop/JModel.cs
+++ src/Xamarin.Forms.Dynamic.Desktop/JModel.cs
@@ -1,39 +1,71 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Reflection;
+using System.Linq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Xamarin.Forms.Dynamic;
 
 namespace Xamarin.Forms
 {
+	/// <summary>
+	/// Dynamic data-bindable JSON-based model.
+	/// </summary>
 	public class JModel : JObject, IReflectableType
 	{
+		static readonly FieldInfo propertiesField = typeof (JObject).GetField ("_properties", BindingFlags.NonPublic | BindingFlags.Instance);
+
+		ObservableCollection<JToken> children;
+		IList<JToken> baseChildren;
+
+		/// <summary>
+		/// Creates the JSON-based model from content 
+		/// objects from Linq to Json content.
+		/// </summary>
 		public JModel (params object[] content)
 		{
+			children = new ObservableCollection<JToken> ();
+			baseChildren = (IList<JToken>)propertiesField.GetValue (this);
+			children.CollectionChanged += new NotifyCollectionChangedEventHandler (OnChanged);
+
+			Add (content);
 		}
 
+		/// <summary>
+		/// Creates the JSON-based model from a loaded  
+		/// Linq to Json object.
+		/// </summary>
 		public JModel (JObject other)
 		{
+			children = new ObservableCollection<JToken> ();
+			baseChildren = (IList<JToken>)propertiesField.GetValue (this);
+			children.CollectionChanged += new NotifyCollectionChangedEventHandler (OnChanged);
+
+			foreach (JToken token in (IEnumerable<JToken>)other) {
+				Add (token);
+			}
 		}
 
 		/// <summary>
-		/// Load a Newtonsoft.Json.Linq.JObject from a string that contains JSON.
+		/// Load a model from a string that contains JSON.
 		/// </summary>
-		/// <param name="json">A System.String that contains JSON.</param>
-		/// <returns>A Newtonsoft.Json.Linq.JObject populated from the string that contains JSON.</returns>
+		/// <param name="json">A string that contains JSON.</param>
+		/// <returns>A <see cref="JModel"/> populated from the string that contains JSON.</returns>
 		public new static JModel Parse (string json)
 		{
-			throw new NotImplementedException ();
+			return new JModel (JObject.Parse (json));
 		}
 
 		/// <summary>
-		/// Loads an Newtonsoft.Json.Linq.JObject from a Newtonsoft.Json.JsonReader.
+		/// Loads an model from a JSON reader.
 		/// </summary>
-		/// <param name="reader">A Newtonsoft.Json.JsonReader that will be read for the content of the Newtonsoft.Json.Linq.JObject.</param>
-		/// <returns>A Newtonsoft.Json.Linq.JObject that contains the JSON that was read from
-		/// the specified Newtonsoft.Json.JsonReader.</returns>
-		public new static JObject Load (JsonReader reader)
+		/// <param name="reader">A <see cref="JsonReader "/> that will be read for the content of the <see cref="JModel"/>.</param>
+		/// <returns>A <see cref="JModel"/> that contains the JSON that was read from the specified <see cref="JsonReader"/>.</returns>
+		public new static JModel Load (JsonReader reader)
 		{
-			throw new NotImplementedException ();
+			return new JModel (JObject.Load (reader));
 		}
 
 		/// <summary>
@@ -41,7 +73,38 @@ namespace Xamarin.Forms
 		/// </summary>
 		public TypeInfo GetTypeInfo ()
 		{
-			throw new NotImplementedException ();
+			return new JTypeInfo (this);
+		}
+
+		protected override IList<JToken> ChildrenTokens
+		{
+			get
+			{
+				return this.children;
+			}
+		}
+
+		void OnChanged (object sender, NotifyCollectionChangedEventArgs e)
+		{
+			if (e.Action == NotifyCollectionChangedAction.Add) {
+				var properties = e.NewItems
+					.OfType<JProperty> ()
+					.Where (prop => prop.Value.Type == JTokenType.Object &&
+						!(prop.Value.Value<JObject> () is JModel));
+
+				foreach (JProperty property in properties) {
+					property.Value = new JModel (property.Value.Value<JObject> ());
+				}
+
+				foreach (JToken token in e.NewItems.OfType<JToken> ()) {
+					baseChildren.Add (token);
+				}
+
+			} else if (e.Action == NotifyCollectionChangedAction.Remove) {
+				foreach (JToken token in e.OldItems.OfType<JToken> ()) {
+					baseChildren.Remove (token);
+				}
+			}
 		}
 	}
 }
---
+++ src/Xamarin.Forms.Dynamic/JModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Xamarin.Forms
+{
+	/// <summary>
+	/// Dynamic data-bindable JSON-based model.
+	/// </summary>
+	public class JModel : JObject, IReflectableType
+	{
+		/// <summary>
+		/// Creates the JSON-based model from content 
+		/// objects from Linq to Json content.
+		/// </summary>
+		public JModel (params object[] content)
+		{
+		}
+
+		/// <summary>
+		/// Creates the JSON-based model from a loaded  
+		/// Linq to Json object.
+		/// </summary>
+		public JModel (JObject other)
+		{
+		}
+
+		/// <summary>
+		/// Load a model from a string that contains JSON.
+		/// </summary>
+		/// <param name="json">A string that contains JSON.</param>
+		/// <returns>A <see cref="JModel"/> populated from the string that contains JSON.</returns>
+		public new static JModel Parse (string json)
+		{
+			throw new NotImplementedException ();
+		}
+
+		/// <summary>
+		/// Loads an model from a JSON reader.
+		/// </summary>
+		/// <param name="reader">A <see cref="JsonReader "/> that will be read for the content of the <see cref="JModel"/>.</param>
+		/// <returns>A <see cref="JModel"/> that contains the JSON that was read from the specified <see cref="JsonReader"/>.</returns>
+		public new static JModel Load (JsonReader reader)
+		{
+			throw new NotImplementedException ();
+		}
+
+		/// <summary>
+		/// Retrieves an object that represents the type of this instance.
+		/// </summary>
+		public TypeInfo GetTypeInfo ()
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
