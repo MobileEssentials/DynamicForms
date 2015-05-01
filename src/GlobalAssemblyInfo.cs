7169d1fcca41e548623f46f2374bb23bc24c2a1b
Daniel Cazzulino <daniel@cazzulino.com>
2015-05-01 02:31:16 -0300

    Factor out shared AssemblyInfo into GlobalAssemblyInfo

---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyCompany("Mobile Essentials")]
+[assembly: AssemblyProduct("Xamarin.Forms.Dynamic")]
+[assembly: AssemblyCopyright("Copyright © Mobile Essentials 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyTitle("Xamarin.Forms.Dynamic")]
+[assembly: AssemblyDescription("Provides dynamic loading of XAML onto views, as well as dynamic JSON-based view models.")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#else
+[assembly: AssemblyConfiguration("RELEASE")]
+#endif
